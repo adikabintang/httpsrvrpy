b93ee4552cb9c351d3cbcab6a226441241602cc3
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2020-07-23 22:27:56 +0300

    added docs for handler.py

--- httpsrvpy/handler.py
+++ httpsrvpy/handler.py
@@ -1,6 +1,27 @@
 from abc import ABC, abstractmethod
 
 class MyHandler(ABC):
+    """
+    class MyHandler is the generic class for handling incoming request to the
+    TCP server.
+
+    The custom TCP request handler class must inherit from this class. For 
+    example, the class MyHttpServer inherits from this class and override the 
+    function `handle(raw)`.
+
+    Methods
+    -------
+    handle(raw)
+        Define what to do upon receiving a request from a client.
+    """
     @abstractmethod
     def handle(self, raw):
+        """
+        Define what to do upon receiving a request from a client.
+
+        Parameters
+        ----------
+        raw : bytearray
+            The data sent by the client.
+        """
         pass
