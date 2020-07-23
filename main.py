b5818615899d16286400371d46461639f0b15c8d
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2020-07-23 21:52:20 +0300

    changed to defaul port 8080

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from httpsrvpy import http_server, tcp_server
 
-my_server = tcp_server.MyTCPServer(http_server.MyHttpServer(), "127.0.0.1", 9999)
+my_server = tcp_server.MyTCPServer(http_server.MyHttpServer())
 my_server.start()
